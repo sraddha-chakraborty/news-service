1a3ce66b61c92cbef736536b50b8253d49a8e7e7
attyuttam <attyuttam@gmail.com>
2023-03-26 18:49:02 +0530

    Completed phase 1

---
+++ src/main/java/com/project/newsservice/application/news/NewsService.java
@@ -0,0 +1,20 @@
+package com.project.newsservice.application.news;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.project.newsservice.domain.News;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class NewsService {
+    private final ThirdPartyNewsService thirdPartyNewsService;
+
+    public List<News> getAllNews(){
+        //Return news by fetching from Google News API
+        return thirdPartyNewsService.getAllNews();
+    }
+}
---
+++ src/main/java/com/project/newsservice/domain/News.java
@@ -0,0 +1,18 @@
+package com.project.newsservice.domain;
+
+import java.util.Date;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class News {
+    private String heading;
+    private String content;
+    private String source;
+    private Date publishDate;
+    private String author;
+}
